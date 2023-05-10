c5b8fb309e4d28ce611de84160afe8660460b20d
Tireddd <dogmen9a@gmail.com>
2023-05-11 00:43:27 +0700

    .

--- Monopoly/src/DefaultSetting.java
+++ Monopoly/src/DefaultSetting.java
@@ -1,2 +1,8 @@
 public class DefaultSetting {
+    public String playerAvatar = "hat.png";
+    public String color = "Black";
+    public String map = "Southside";
+    public int volume = 50;
+    public int numPlayers = 4;
+    public int numTurns = 30;
 }
