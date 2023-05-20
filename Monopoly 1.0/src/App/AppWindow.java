c1698d492247f32b5b02f9a2cc01481ffeedf7e9
Quân Nguyễn <125657444+ITDSIU20094@users.noreply.github.com>
2023-05-20 14:21:32 +0700

    update

--- Monopoly 1.0/src/App/AppWindow.java
+++ Monopoly 1.0/src/App/AppWindow.java
@@ -1,42 +1,62 @@
 package App;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class AppWindow extends JFrame {
+        public AppWindow() {
+                ImageIcon imageIcon = new ImageIcon("Image/demo.png");
+                JLabel imageLabel = new JLabel(imageIcon);
+                imageLabel.setBounds(0, 0, imageIcon.getIconWidth(), imageIcon.getIconHeight());
 
-public class AppWindow {
-    private JFrame jFrame;
-    private ImageIcon imageIcon;
-    private JLabel jLabel;
-    private JButton startButton;
-    private JButton settingButton;
+                JButton startButton = new JButton("Start Game");
+                startButton.setBounds(450, 250, 100, 40);
+                startButton.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                                setVisible(false);
+                                dispose();
+                                EventQueue.invokeLater(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                                new joinGame().setVisible(true);
+                                        }
+                                });
+                        }
+                });
 
-    public AppWindow(){
+                JButton settingButton = new JButton("Setting");
+                settingButton.setBounds(450, 300, 100, 40);
+                settingButton.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                                EventQueue.invokeLater(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                                new AppSetting().setVisible(true);
+                                        }
+                                });
+                        }
+                });
 
-        imageIcon = new ImageIcon("Image/demo.png");
-        jLabel = new JLabel(imageIcon);
-        jLabel.setSize(1000,563);
+                JLayeredPane layeredPane = new JLayeredPane();
+                layeredPane.setPreferredSize(new Dimension(imageIcon.getIconWidth(), imageIcon.getIconHeight()));
+                layeredPane.add(imageLabel, Integer.valueOf(0));
+                layeredPane.add(startButton, Integer.valueOf(1));
+                layeredPane.add(settingButton, Integer.valueOf(1));
 
-        startButton = new JButton("Start");
-        startButton.setBounds(450, 250, 100, 40);
-
-
-        settingButton = new JButton("Setting");
-        settingButton.setBounds(450, 300, 100, 40);
-        settingButton.addActionListener(e -> settingGame());
-
-        jFrame = new JFrame();
-        jFrame.add(jLabel);
-        jFrame.add(startButton);
-        jFrame.add(settingButton);
-        jFrame.setSize(1000,563);
-        jFrame.setLayout(new BorderLayout());
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setLocationRelativeTo(null);
-        jFrame.setVisible(true);
-    }
-
-    public void settingGame(){
-        new AppSetting();
-    }
-
+                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                setSize(imageIcon.getIconWidth(), imageIcon.getIconHeight());
+                setLocationRelativeTo(null);
+                getContentPane().add(layeredPane);
+        }
 }
+
+
+
+
+
+
+
+
--- Monopoly 1.0/src/Main.java
+++ Monopoly 1.0/src/Main.java
@@ -1,7 +1,15 @@
 import App.AppWindow;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        new AppWindow();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                AppWindow appWindow = new AppWindow();
+                appWindow.setVisible(true);
+            }
+        });
     }
 }
